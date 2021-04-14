1af9747af8dac0f5e7765f5e1bec76c3b9243f0a
Oscar Olivera <oscarmolivera@gmail.com>
2021-04-14 17:11:47 -0400

    Solution1: Comentaries and Assumptions

--- Solution_1/mars_rover.rb
+++ Solution_1/mars_rover.rb
@@ -1,21 +1,24 @@
 #!/usr/bin/env ruby
 
 =begin
+Design: for this solution I decided to divide the problem into two sections.
+Reading the data and a Rover class that is used to instantiate various rover
+objects that have primarily "move" and "rotate" methods.
 
-A rover's position and location is represented by a combination of x and y
-coordinates and a letter representing one of the four cardinal compass points.
-The plateau is divided up into a grid to simplify navigation. An example
-position might be 0, 0, N, which means the rover is in the bottom left corner
-and facing North.
+Assumptions: The ruby version used is 2.4.1.
 
-In order to control a rover, NASA sends a simple string of letters. The possible
-letters are 'L', 'R' and 'M'. 'L' and 'R' makes the rover spin 90 degrees left
-or right respectively, without moving from its current spot. 'M' means move
-forward one grid point, and maintain the same heading.
+Instructions:
+Run this command in terminal:
+ruby mars_rover.rb <path-to-data-file / data.tx>
 
-Assume that the square directly North from (x, y) is (x, y+1).
+A file with the same data format delivered in the test document must be provided
+to compute the address of the rover.
+
+The program prints on the screen a different response format than the one
+requested in the test
 
 =end
+# Rover Class 
 class Rover
   def initialize(initial_position, plateau)
     @grid_size = plateau.map(&:to_i)
@@ -23,11 +26,12 @@ class Rover
     @coordinates = conversion[0..1].map(&:to_i)
     @rover_facing = conversion.last.capitalize
   end
-
+  # List of all the commands for the rover to explore
   def commands(list)
     @commands = list.chars
   end
 
+  # Navigate between all the commands and move or rotate accordingly
   def explore
     @commands.each do |command|
       break unless out_of_bounds?
@@ -40,11 +44,13 @@ class Rover
 
   private
 
+  # Using the class variable to turn left or right
   def rotate(command)
     turn = directions[@rover_facing]
     @rover_facing = command == 'L' ? turn.first : turn.last
   end
 
+  # Move the rover on the grid based on the cardinal point it is facing
   def move
     case @rover_facing
     when 'N'
@@ -58,11 +64,14 @@ class Rover
     end
   end
 
+  # Validate if the rover moved outside the grid.
   def out_of_bounds?
     (0..@grid_size[0]).cover?(@coordinates[0]) &&
       (0..@grid_size[1]).cover?(@coordinates[1])
   end
 
+  # Hash that allows to rotate. A cardinal point has an array
+  # array[0] is a left turn, array[1] is a right turn.
   def directions
     { 'N' => %w[W E],
       'E' => %w[N S],
@@ -78,14 +87,19 @@ if File.exist?(ARGV[0])
     puts '................. Explore Mars with Rovers.......................'
     puts ''
     File.foreach(ARGV[0]) do |line|
+      # Grid variable for rover to expllore
       @plateau = line[/(\d+\s\d+)\n/, 1].split(' ') unless line[/(\d+\s\d+)\n/, 1].nil?
       case line
       when /(\d+\s\d+\s[neswNESW])\n/
+        # Initial coordenate of the rover
         @position_line = (line[/(\d+\s\d+\s[neswNESW])/])
       when /[MRL]+/
+        # Initializating the rover
         rover = Rover.new(@position_line, @plateau)
+        # Sending commands 
         rover.commands(line[/[MRL]+/])
         @num += 1
+        # Exploring the plateau and printing results
         puts "Rover#{@num} Initial position: #{@position_line} | #{rover.explore}"
         puts '_____________________________________________________'
       else
