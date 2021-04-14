907646bad92ed8dbfad6b98a565097fae9523bce
Oscar Olivera <oscarmolivera@gmail.com>
2021-04-14 13:05:27 -0400

    FILE_Init: simple layout

---
+++ mars_rover.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+=begin
+
+A rover's position and location is represented by a combination of x and y
+coordinates and a letter representing one of the four cardinal compass points.
+The plateau is divided up into a grid to simplify navigation. An example
+position might be 0, 0, N, which means the rover is in the bottom left corner
+and facing North.
+
+In order to control a rover, NASA sends a simple string of letters. The possible
+letters are 'L', 'R' and 'M'. 'L' and 'R' makes the rover spin 90 degrees left
+or right respectively, without moving from its current spot. 'M' means move
+forward one grid point, and maintain the same heading.
+
+Assume that the square directly North from (x, y) is (x, y+1).
+
+=end
+
+
+puts '===================== Checkr Assessment ========================='
+puts '......................... Mars Rover ............................'
+puts ''
